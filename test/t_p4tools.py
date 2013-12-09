fd9350e4df3641fda3fce5fed26bcf6d60122b92
Michael Schober <mschober@BELC02KC1WLFFT3.sea.corp.expecn.com>
2013-12-09 12:47:35 -0800

    convoluted way to test p4 dirs works

--- test/t_p4tools.py
+++ test/t_p4tools.py
@@ -2,10 +2,11 @@
 
 from nose.tools import istest
 import src.p4tools as p4tools
+import unittest
 
-class Testp4:
+class Testp4(unittest.TestCase):
 
     @istest
     def dirs_cmd_happy_path(self):
-        p4 = p4tools.p4('//depot/dmo')
-        assert 'dmo' in p4.dirs()[0].split('/')
+        p4 = p4tools.p4('//depot/dmo/*')
+        self.assertEquals('', p4.dirs()[0].split('//depot/dmo')[0])
