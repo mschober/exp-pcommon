2f1ae1d9336b19c38da9588a0726ba06b12f433b
Michael Schober <mschober@BELC02KC1WLFFT3.sea.corp.expecn.com>
2013-12-09 13:36:33 -0800

    path vars

--- test/t_p4tools.py
+++ test/t_p4tools.py
@@ -8,10 +8,12 @@ class Testp4(unittest.TestCase):
 
     @istest
     def dirs_cmd_happy_path(self):
-        p4 = p4tools.p4('//depot/dmo/*')
-        self.assertEquals('', p4.dirs()[0].split('//depot/dmo')[0])
+        path = '//depot/dmo/*'
+        p4 = p4tools.p4(path)
+        self.assertEquals('', p4.dirs()[0].split(path[:-2])[0])
 
     @istest
     def files_cmd_happy_path(self):
-        p4 = p4tools.p4('//depot/dmo/BookingImp/BookingImp/DEV/db/tbl/index/...')
-        self.assertEquals('', p4.files()[0].split('//depot/dmo/BookingImp/BookingImp/DEV/db/tbl/index')[0])
+        path = '//depot/dmo/BookingImp/BookingImp/DEV/db/tbl/index/...'
+        p4 = p4tools.p4(path)
+        self.assertEquals('', p4.files()[0].split(path[:-4])[0])
