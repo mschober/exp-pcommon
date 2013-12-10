a9098af81ea9bb421ec7bbcd5fa82e1799bd031f
Michael Schober <mschober@BELC02KC1WLFFT3.sea.corp.expecn.com>
2013-12-10 15:57:04 -0800

    replace header logic moved here for testing

--- src/perforce_header.py
+++ src/perforce_header.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 
-class PerforceHeader:
+class Document:
 
-    def __init__(self, header, **kwargs):
-        self.header = header.format(**kwargs)
-        self.kwargs = kwargs
+    def __init__(self, text):
+        #self.text = text.format(**kwargs)
+        self.text = text
 
     def __str__(self):
-        return self.header
+        return self.text
 
     def lst(self):
-        return self.header.split('\n')
+        return self.text.split('\n')
+
+    def replace_header(self, new_header, split_string, **kwargs):
+        rebuilt_file = []
+        blocks = self.text.split(split_string)
+
+        header_string = blocks[0]
+        body_lst = ''.join(blocks[1:]).split('\n')
+
+        header_string = new_header.format(**kwargs)
+        header_lst = header_string.split('\n')
+        header_lst.append(split_string)
+
+        rebuilt_file.extend(header_lst)
+        rebuilt_file.extend(body_lst)
+        self.text = "\n".join(rebuilt_file)
--- test/t_perforce_header.py
+++ test/t_perforce_header.py
@@ -5,7 +5,7 @@ from nose.tools import istest
 import src.perforce_header as perforce_header
 import re
 
-class TestPerforceHeader(unittest.TestCase):
+class TestDocument(unittest.TestCase):
 
     p4_header = '''/******************************************************************************
 Copyright 2013 Expedia, Inc.
@@ -22,13 +22,19 @@ Change History:
 
     @istest
     def my_example(self):
-        header = perforce_header.PerforceHeader(self.p4_header, file_name='my_file.sql', current_date='2013')
+        doc = perforce_header.Document('line1\nline2\nline3')
+        doc.replace_header(self.p4_header, '1', file_name='my_file.sql', current_date='2013-12-10')
         has_file = False
         has_date = False
-        for line in header.lst():
+        for line in doc.lst():
             if re.search('my_file.sql', line):
                 has_file = True
-        for line in header.lst():
+        for line in doc.lst():
             if re.search('2013', line):
                 has_date = True
         assert has_file and has_date
+
+    @istest
+    def new_document(self):
+        doc = perforce_header.Document('line1\nline2\nline3')
+        self.assertEquals(['line1', 'line2', 'line3'], doc.lst())
