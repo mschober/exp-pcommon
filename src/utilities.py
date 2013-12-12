a71f5478b2f4132c3b957192740c096a7670fe0e
Michael Schober <mschober@BELC02KC1WLFFT3.sea.corp.expecn.com>
2013-12-11 16:09:24 -0800

    adding full_paths

--- src/utilities.py
+++ src/utilities.py
@@ -4,5 +4,8 @@ def lowercase(x):
     return map(str.lower, x)
 
 def full_path(*args):
-    return "/".join(args)
+    return "/".join(*args)
+
+def full_paths(path_list):
+    return map(full_path, path_list)
 
