b112f5970539fd9196c4fdf20445af6c727bf99e
Michael Schober <mschober@BELC02KC1WLFFT3.sea.corp.expecn.com>
2013-12-13 09:22:08 -0800

    keep track of missing files, stub in no header insert

--- src/perforce_header.py
+++ src/perforce_header.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import common.src.fileutil as fileutil
+import re
 
 class Document:
 
@@ -20,26 +21,18 @@ class Document:
     def file_name(self):
         return fileutil.path_tail(self.path)
 
-    def p4_path(self):
-        return self.path
-
     def remove_root(self, root):
         fname = fileutil.path_tail(self.path)
         self.path = self.path.replace(root + '/', '')
         self.path = self.path.replace('/' + fname, '')
 
-    def __replace_header(self, new_header, split_line, upper=False, **kwargs):
+    def missing(self):
+        return self.no_match
+
+    def __replace_header(self, new_header, split_line=None, upper=False, **kwargs):
         rebuilt_file = []
 
-        if upper:
-            split_line = str.upper(split_line)
-
-        if split_line in self.lst():
-            blocks = self.__blocks(split_line)
-
-            header_string = blocks[0]
-            body_lst = ''.join(blocks[1:]).split('\n')
-
+        def insert_header(new_header, **kwargs):
             header_string = new_header.format(**kwargs)
             header_lst = header_string.split('\n')
             header_lst.append(split_line)
@@ -47,10 +40,32 @@ class Document:
             rebuilt_file.extend(header_lst)
             rebuilt_file.extend(body_lst)
             self.text = "\n".join(rebuilt_file)
+
+
+        if not split_line:
+            pass
+            #insert_header(new_header, **kwargs)
+
+        if upper:
+            split_line = str.upper(split_line)
+
+        if split_line in self.lst():
+            blocks = self.__blocks(split_line)
+
+            if len(blocks[0]) < 3 and re.search(r'/' + 5 * '\*', blocks[1]):
+                print 'split wrong, blocks (%s) path (%s)' % (blocks[0], self.path)
+
+            header_string = blocks[0]
+            body_lst = ''.join(blocks[1:]).split('\n')
+
+            insert_header(new_header, **kwargs)
+            return False
         else:
-            self.no_match.append(self.text)
+            return True
 
 
     def replace_header(self, new_header, split_line, **kwargs):
-        self.__replace_header(new_header, split_line, **kwargs)
-        self.__replace_header(new_header, split_line, upper=True, **kwargs)
+        missing = self.__replace_header(new_header, split_line, **kwargs)
+        missing = missing and self.__replace_header(new_header, split_line, upper=True, **kwargs)
+        if missing:
+            self.no_match = (self.path, self.text)
