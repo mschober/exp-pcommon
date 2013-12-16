6eaa57911cb7e7ae349dfc61e898f13ec1e1e341
Michael Schober <mschober@BELC02KC1WLFFT3.sea.corp.expecn.com>
2013-12-16 10:46:48 -0800

    flowerbox matching pattern

--- test/t_perforce_header.py
+++ test/t_perforce_header.py
@@ -39,3 +39,19 @@ Change History:
     def new_document(self):
         doc = perforce_header.Document('path', 'line1\nline2\nline3')
         self.assertEquals(['line1', 'line2', 'line3'], fileutil.blocks(str(doc)))
+
+    @istest
+    def has_flower_box(self):
+        doc = perforce_header.Document('path', self.p4_header + '\n\nline1\nline2\nline3')
+        begins_with_slash_star_plus_any_number_of_stars = '^/\*(\*)+\n'
+        contains_any_number_of_lines = '(.*\n)+'
+        ends_with_any_number_of_stars_plus_star_slash = '(\*)+\*/$'
+        flower_pattern = begins_with_slash_star_plus_any_number_of_stars + contains_any_number_of_lines + ends_with_any_number_of_stars_plus_star_slash
+
+        matching_patterns = ['/**\n header line one \n**/', '/**********\n header line 1 \n header line 2 \n**************/']
+        not_matching_patterns = ['', '*', '/* block comment \n line2 */']
+        print flower_pattern
+        for txt in matching_patterns:
+            assert re.search(flower_pattern, txt), '(' + str(txt) + ')'
+        for txt in not_matching_patterns:
+            assert not re.search(flower_pattern, txt), '(' + str(txt) + ')'
