d41bbb23f16453501d290aabc7149f291753aba0
Michael Schober <mschober@expedia.com>
2014-02-03 12:16:11 -0800

    adding code from common package (why was this not here? )

---
+++ common/utilities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import subprocess
+
+def lowercase(x):
+    return map(str.lower, x)
+
+def full_path(*args):
+    return "/".join(*args)
+
+def full_paths(path_list):
+    return map(full_path, path_list)
+
+def execute_command(cmd):
+    output = subprocess.check_output(cmd.split())
+    if len(output) > 1:
+        if not output[-1] == '\n':
+            output += '\n'
+    results = output.split('\n')
+    results.pop()
+    return results
+
+def get_user():
+    return execute_command('whoami')[0]
